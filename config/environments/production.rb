85a6a34eeb168cb5000e5a16b3b004b1511110b1
Jun Lim <junyuen@gmail.com>
2013-10-27 11:41:37 +0800

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Four::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku
+  # Note to set this to Actual Host
+  config.action_mailer.default_url_options = { :host => 'http://empat.herokuapp.com/' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Four::Application.routes.draw do
+
+  devise_for :users
   root "pages#index"
   get "about" => "pages#about"
   get "contact" => "pages#contact"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
